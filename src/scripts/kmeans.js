3d066ad96bce818061c75188aa72d9a79c51651b
Graciele Rodrigues <83515246+GracieleRodrigues-dev@users.noreply.github.com>
2024-05-05 18:05:20 -0300

    feat: implement k-means  model

--- src/scripts/kmeans.js
+++ src/scripts/kmeans.js
@@ -1,40 +1,75 @@
 import { getKmeansDataset } from '../utils/functions.js';
 
-const dataset = getKmeansDataset('area', 'perimetro');
-let calculated = false;
+let kmeansModel;
 
-const options = {
-  k: 4,
-  maxIter: 4,
-  threshold: 0.5
-};
+const width = 640;
+const height = 480;
 
-// Initialize kmeans
-const kmeans = ml5.kmeans(dataset, options, clustersCalculated);
+// Add event listener to the submit button
+document.getElementById("clusterButton").addEventListener("click", make);
 
-// When the model is loaded
-function clustersCalculated() {
-  calculated = true;
-  console.log('Points Clustered!');
-  console.log(calculated);
-  console.log(kmeans.dataset);
-  //console.log(kmeans.centroids);
+// create the model
+function make() {
+  const currentClusterCount = parseInt(document.getElementById("clusterInput").value);
+  const options = {
+    'k': currentClusterCount,
+    'maxIter': 10,
+    'threshold': 2,
+  };
+
+  const data = getKmeansDataset('area', 'perimetro');
+  console.log("iniciando o kmeans");
+  kmeansModel = ml5.kmeans(data, options, modelReady);
 }
 
-/*
-function draw() {
-  if (calculated) {
-    for (i = 0; i < kmeans.dataset.length; i++) {
-      let centroid = kmeans.dataset[i].centroid;
-      let datapointx = kmeans.dataset[i][0];
-      let datapointy = kmeans.dataset[i][1];
-      //We are using the HSB colorMode heres
-      fill(centroid * 36, 100,100);
-      ellipse(datapointx, datapointy, 20, 20);
-      //We also add a label to the output, so it could be interpreted without the color
-      fill(0);
-      textAlign(CENTER, CENTER);
-      text(centroid + 1, datapointx, datapointy);
-    }
-  }
-}*/
+// when the model is ready, make the chart
+function modelReady() {
+  console.log("Clusters calculados!");
+  makeChart()
+}
+
+// use the fancy d3 to make magic
+function makeChart() {
+  const dataset = kmeansModel.dataset;
+  d3.select('svg').remove();
+
+  // reappend the svg to the chart area
+  const svg = d3.select('#chart').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  // d[0] is for the x value in the array
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, d => d[0]))
+    .range([10, width - 100]);
+
+  // d[1] is for the y value in the array
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, d => d[1]))
+    .range([height - 50, 20]);
+
+  svg.selectAll('circle').data(dataset)
+    .enter().append('circle')
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', 6)
+    .attr('fill', 'black')
+
+  d3.select('svg').selectAll('circle')
+    .transition()
+    .attr('fill', (d, i) => createRGB(dataset[i].centroid))
+}
+
+// Função para gerar uma cor RGB com base no valor do centroide
+function createRGB(centroid) {
+  // Calcular o deslocamento baseado no valor do centroid
+  const offset = centroid * 30; // Ajuste conforme necessário para a distância desejada entre as cores
+
+  // Calcular os componentes RGB com base no deslocamento
+  const r = (centroid * 53 + offset) % 255; // calcular R
+  const g = (centroid * 101 + offset) % 255; // calcular G
+  const b = (centroid * 157 + offset) % 255; // calcular B
+
+  // Retorna a cor RGB no formato 'rgb(x, y, z)'
+  return `rgb(${r}, ${g}, ${b})`;
+}
