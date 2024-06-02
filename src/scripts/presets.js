4f684d601ec648e5ad77e317660baa8b4572c7de
Graciele Rodrigues <83515246+GracieleRodrigues-dev@users.noreply.github.com>
2024-06-02 20:44:06 -0300

    feat: presets.js

---
+++ src/scripts/presets.js
@@ -0,0 +1,7 @@
+// presets.js
+export const presets = {
+    "Dimensões": ["area", "perimetro", "eixo_maior", "eixo_menor", "extensao", "redondidade", "compactidade"],
+    "Forma": ["fator_forma_1", "fator_forma_2", "fator_forma_3", "fator_forma_4"],
+    "Coloração": ["RR_all", "RG_all", "RB_all"]
+  };
+  
